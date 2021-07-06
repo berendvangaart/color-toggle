d32cd52cef950912bfc12eae4af008688d0d7434
Berend <berendvangaart@gmail.com>
2021-07-06 17:42:57 +0200

    change styling + comments

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("color-list");
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('color-list');
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 })
 
 menu.addEventListener('click', e => {
-    document.body.style.background = e.target.classList[1];
-    document.getElementById('color-name').innerText = e.target.id;
-    menu.classList.toggle('hidden');
+    document.body.style.background = e.target.classList[1];         // set color to the corresponding hex color
+    document.getElementById('color-name').innerText = e.target.id;  // display color name on page
+    menu.classList.toggle('hidden');                                // hide menu
 })
