ab01ef46f0234a777f67b447917b2c3133f965d7
Berend <berendvangaart@gmail.com>
2021-07-06 17:14:17 +0200

    final color-toggle

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("color-list");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle('hidden');
+})
+
+menu.addEventListener('click', e => {
+    document.body.style.background = e.target.classList[1];
+    document.getElementById('color-name').innerText = e.target.id;
+    menu.classList.toggle('hidden');
+})
